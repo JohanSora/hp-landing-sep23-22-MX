d1948974cc517b1b1cdc307f684eb5ab99ab10d1
JohanSora <johan.sora@mymarketlogic.com>
2022-09-23 10:24:18 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="container-global-header">
+      <div className="upper-logos"></div>
+      <div className="container-info-form"></div>
+    </div>
+  );
+};
+
+export default Header;
