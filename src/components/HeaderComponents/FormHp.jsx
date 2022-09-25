308e4d3802e237a5b5a792d642001fd1c61b1d47
Johan0305 <johansp0305@gmail.com>
2022-09-25 12:23:07 -0500

    add final design

--- src/components/HeaderComponents/FormHp.jsx
+++ src/components/HeaderComponents/FormHp.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const FormHp = () => {
-  return <div></div>;
+  return (
+    <form onSubmit={() => console.log("aaaaaaaa")}>
+      <p>Regístrese ahora</p>
+      <label htmlFor="text">
+        Nombre:
+        <input type="text" />
+      </label>
+      <label htmlFor="text">
+        Apellidos:
+        <input type="text" />
+      </label>
+      <label htmlFor="text">
+        Empresa:
+        <input type="text" />
+      </label>
+      <label htmlFor="text">
+        Cargo:
+        <input type="text" />
+      </label>
+      <label htmlFor="text">
+        Correo empresarial:
+        <input type="text" />
+      </label>
+      <label htmlFor="text">
+        Teléfono Móvil:
+        <input type="text" />
+      </label>
+      <div className="captcha-and-button">
+        <button type="submit">ENVIAR</button>
+      </div>
+    </form>
+  );
 };
 
 export default FormHp;
